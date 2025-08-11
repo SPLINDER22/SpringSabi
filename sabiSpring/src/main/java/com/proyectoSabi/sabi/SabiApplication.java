be41525c66a4aeaa0ca368bdaf1aa7af56bac99b
cubeblack22 <rojasmena1222@gmail.com>
2025-08-11 07:00:57 -0500

    subida
    se creo la carpeta de sabi en springboot

---
+++ sabiSpring/src/main/java/com/proyectoSabi/sabi/SabiApplication.java
@@ -0,0 +1,13 @@
+package com.proyectoSabi.sabi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SabiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SabiApplication.class, args);
+	}
+
+}
---
+++ sabiSpring/src/test/java/com/proyectoSabi/sabi/SabiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.proyectoSabi.sabi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SabiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
